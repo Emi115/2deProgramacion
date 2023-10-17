79debbeac157b2178bc5917bb0ef51bf8fba6e3a
Emi115 <emiomg8@gmail.com>
2023-10-17 17:24:16 +0200

    asd
    asd

--- Gabri/Parte7/ejerciciofaker/ejercicio1.mjs
+++ Gabri/Parte7/ejerciciofaker/ejercicio1.mjs
@@ -1,11 +1,21 @@
-import chalk from 'chalk'
-import { DateTime } from 'luxon'
+import chalk from 'chalk';
+import { DateTime } from 'luxon';
 
-const currentTime = DateTime.local()
-const formattedTime = currentTime.toFormat('HH:mm:ss')
+const colors = [chalk.red, chalk.green, chalk.blue, chalk.yellow, chalk.cyan];
 
-const colors = [chalk.red, chalk.green, chalk.blue, chalk.yellow, chalk.underline, chalk.bgBlackBright, chalk.bgCyanBright, chalk.cyan]
+function printFormattedTime() {
+  const currentTime = DateTime.local();
+  const formattedTime = currentTime.toFormat('hh:mm:ss a'); // Formato con AM/PM
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  process.stdout.write('\x1B[2J\x1B[0f'); // Borra la pantalla y coloca el cursor en la parte superior izquierda.
+  console.log(randomColor(`Hora actual : ${formattedTime}`));
+}
 
-colors.forEach((color) => {
-  console.log(color(`Hora actual : ${formattedTime}`))
-})
+setInterval(printFormattedTime, 1000);
+
+setTimeout(() => {
+  console.log("Fin del programa.");
+  process.exit(0);
+}, 5 * 60 * 1000);
+
+
